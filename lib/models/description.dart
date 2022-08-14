eda6b0fa4d2927ea379cb2e0fbbb2ff9a2e9caa4
Mameng57 <takiya2888157@gmail.com>
2022-08-14 23:25:17 +0700

    create description model

---
+++ lib/models/description.dart
@@ -0,0 +1,11 @@
+class Description {
+  const Description({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String image;
+  final String title;
+  final String subtitle;
+}
