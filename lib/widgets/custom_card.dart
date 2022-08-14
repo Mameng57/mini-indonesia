6e107543b6ab3fd7f055f11234349644c98bb53a
Mameng57 <takiya2888157@gmail.com>
2022-08-14 23:25:30 +0700

    custom card for landing page

---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:lovo_photography/models/description.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard(this._size, this._color, this._description, {Key? key})
+      : super(key: key);
+
+  final Map<String, double> _size;
+  final Color _color;
+  final Description _description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: _size['width']! / 1.3,
+      height: _size['height']! / 4.5,
+      decoration: BoxDecoration(
+        color: _color.withAlpha(100),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+            width: _size['width']! / 5,
+            height: _size['height']! / 10,
+            image: AssetImage(
+              _description.image,
+            ),
+          ),
+          Text(
+            _description.title,
+            style: Theme.of(context).textTheme.headline2?.copyWith(
+              color: _color,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            _description.subtitle,
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+              color: _color,
+            ),
+            textAlign: TextAlign.justify,
+          ),
+        ],
+      ),
+    );
+  }
+}
