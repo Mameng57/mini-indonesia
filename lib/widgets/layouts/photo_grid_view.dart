08d74a8dfe7895548246eeca7522b8cf7bf0c1fa
Mameng57 <takiya2888157@gmail.com>
2023-01-20 13:36:26 +0700

    adds 2 modes, multiselect mode and default view mode

--- lib/widgets/layouts/photo_grid_view.dart
+++ lib/widgets/layouts/photo_grid_view.dart
@@ -2,10 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:lovo_photography/services/base_client.dart';
 import 'package:lovo_photography/models/photo.dart';
 import 'package:lovo_photography/widgets/clickable_image_box.dart';
+import 'package:lovo_photography/widgets/selectable_image_box.dart';
 
-class PhotoGridView extends StatelessWidget {
-  const PhotoGridView(this.listPhoto, {Key? key}) : super(key: key);
+class PhotoGridView extends StatefulWidget {
+  const PhotoGridView({
+    required this.listPhoto,
+    required this.listSelectedPhoto,
+    required this.addSelectedPhotoHandler,
+    required this.removeSelectedPhotoHandler,
+    Key? key
+  }) : super(key: key);
+
   final List<Photo> listPhoto;
+  final List<Photo> listSelectedPhoto;
+  final Function addSelectedPhotoHandler;
+  final Function removeSelectedPhotoHandler;
+
+  @override
+  State<PhotoGridView> createState() => _PhotoGridViewState();
+}
+
+class _PhotoGridViewState extends State<PhotoGridView> {
+  bool isSelectableMode = false;
+  int firstSelection = 0;
+
+  void changeMode() {
+    setState(() {
+      isSelectableMode = !isSelectableMode;
+    });
+  }
+
+  void changeFirstSelection(int index) {
+    setState(() {
+      firstSelection = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +46,35 @@ class PhotoGridView extends StatelessWidget {
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
       ),
-      itemCount: listPhoto.length,
+      itemCount: widget.listPhoto.length,
       itemBuilder: (context, index) {
-        return ClickableImageBox(
-          "${listPhoto[index].idPhoto}",
-          "${BaseClient.apiUrl}/${listPhoto[index].url}",
+        return isSelectableMode
+        ? SelectableImageBox(
+          imageTitle: "${widget.listPhoto[index].idPhoto}",
+          imageUrl: "${BaseClient.apiUrl}/${widget.listPhoto[index].url}",
+          isSelected: index == firstSelection ? true : false,
+          selectedHandler: (bool isSelected) {
+            if (isSelected) {
+              widget.addSelectedPhotoHandler(index);
+            }
+            else {
+              widget.removeSelectedPhotoHandler(index);
+
+              if (widget.listSelectedPhoto.isEmpty) {
+                changeMode();
+              }
+            }
+          },
+          key: Key(widget.listPhoto[index].idPhoto.toString()),
+        )
+        : ClickableImageBox(
+          imageTitle: "${widget.listPhoto[index].idPhoto}",
+          imageUrl: "${BaseClient.apiUrl}/${widget.listPhoto[index].url}",
+          selectedHandler: (_) {
+            widget.addSelectedPhotoHandler(index);
+            changeMode();
+            changeFirstSelection(index);
+          },
         );
       }
     );
