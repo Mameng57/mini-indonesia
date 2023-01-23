8063b7f0d43a21ed3e1ac1034d4c79362bc21ea3
Mameng57 <takiya2888157@gmail.com>
2023-01-23 21:40:22 +0700

    modified session model

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,33 +1,41 @@
 class Session {
   int? idSession;
-  DateTime? date;
-  String? packageInfo;
-  int? photosCount;
-  int? idUser;
+  DateTime? dateTaken;
+  DateTime? dateDue;
+  String? namePackage;
+  int? downloadCount;
+  int? printCount;
+  String? name;
 
   Session({
     this.idSession,
-    this.date,
-    this.packageInfo,
-    this.photosCount,
-    this.idUser,
+    this.dateTaken,
+    this.dateDue,
+    this.namePackage,
+    this.downloadCount,
+    this.printCount,
+    this.name,
   });
 
   Session.fromJson(Map<String, dynamic> json) {
     idSession = json['id_session'];
-    date = DateTime.parse(json['date']);
-    packageInfo = json['package_info'];
-    photosCount = json['photos_count'];
-    idUser = json['id_user'];
+    dateTaken = DateTime.parse(json['date_taken']);
+    dateDue = DateTime.parse(json['date_due']);
+    namePackage = json['name_package'];
+    downloadCount = json['download_count'];
+    printCount = json['print_count'];
+    name = json['name'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id_session'] = this.idSession as int;
-    data['date'] = this.date.toString();
-    data['package_info'] = this.packageInfo;
-    data['photos_count'] = this.photosCount as int;
-    data['id_user'] = this.idUser as int;
+    data['id_session'] = this.idSession;
+    data['date_taken'] = this.dateTaken.toString();
+    data['date_due'] = this.dateDue.toString();
+    data['name_package'] = this.namePackage;
+    data['download_count'] = this.downloadCount;
+    data['print_count'] = this.printCount;
+    data['name'] = this.name;
     return data;
   }
 }
