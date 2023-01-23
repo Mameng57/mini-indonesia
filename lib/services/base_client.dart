f696493dea1a42e8f87475d446f7fd4bb8a2ce78
Mameng57 <takiya2888157@gmail.com>
2023-01-24 05:06:27 +0700

    nambah PUT

--- lib/services/base_client.dart
+++ lib/services/base_client.dart
@@ -25,4 +25,15 @@ class BaseClient {
 
     throw Exception("Gagal memanggil api...");
   }
+
+  Future<dynamic> put(String url, Map<String, dynamic> formData) async {
+    var endpoint = Uri.parse(apiUrl + url);
+
+    var response = await client.put(endpoint, body: formData);
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+
+    return response.statusCode;
+  }
 }
