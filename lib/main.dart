73eac0e4bba59ebc2b4b31f7882ef36d7de2e5cf
Mameng57 <takiya2888157@gmail.com>
2023-01-23 21:41:42 +0700

    use themes from theme.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lovo_photography/theme.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:lovo_photography/models/session.dart';
 import 'package:lovo_photography/models/user.dart';
@@ -13,77 +14,12 @@ void main() => runApp(const LovoApp());
 
 class LovoApp extends StatelessWidget {
   const LovoApp({Key? key}) : super(key: key);
-  final Color _primaryColor = const Color(0xFF567189);
-  final Color _primaryContainerColor = const Color(0xFF7B8FA1);
-  final Color _secondaryColor = const Color(0xFFCFB997);
-  final Color _secondaryContainerColor = const Color(0xFFFAD6A5);
-  final Color _textWhite = const Color(0xFFF7F5EB);
-  final Color _bgColor = const Color(0xFF343F49);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Lovo Photography App",
-      theme: ThemeData.dark().copyWith(
-        primaryColor: _primaryColor,
-        backgroundColor: _bgColor,
-        colorScheme: const ColorScheme.dark().copyWith(
-          primary: _primaryColor,
-          primaryContainer: _primaryContainerColor,
-          onPrimary: _textWhite,
-          secondary: _secondaryColor,
-          secondaryContainer: _secondaryContainerColor,
-          onSecondary: _primaryColor,
-          background: _bgColor,
-        ),
-        scaffoldBackgroundColor: _bgColor,
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 55,
-            fontWeight: FontWeight.w700,
-            height: 1,
-          ),
-          headlineMedium: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-          ),
-          headlineSmall: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            height: 1,
-          ),
-          bodyLarge: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 20,
-            fontWeight: FontWeight.normal,
-          ),
-          bodyMedium: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 18,
-            fontWeight: FontWeight.normal,
-          ),
-          bodySmall: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 16,
-            fontWeight: FontWeight.normal,
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: _secondaryColor,
-          enabledBorder: UnderlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 1.5, color: _primaryColor),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(width: 1.5, color: _primaryColor),
-            borderRadius: BorderRadius.circular(15),
-          ),
-        ),
-      ),
+      theme: customTheme(),
       initialRoute: "/",
       routes: {
         LaunchPage.routeName: (context) => const LaunchPage(),
